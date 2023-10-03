0ac1cafca2df761874dbf9fce09e72fe6ab4bdfe
Lainey <dev.lainey@gmail.com>
2023-10-03 13:45:26 -0400

    add main.jsx content

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
+//----- IMPORTS -----//
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//Pages
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import CityInfo from './pages/Projects.jsx';
+
+//----- EXPORTS -----//
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/cityinfo",
+        element: <CityInfo />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
