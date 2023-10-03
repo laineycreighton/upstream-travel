a848f60eee5f972c53e6fd368e722d7f142465fa
Lainey <dev.lainey@gmail.com>
2023-10-03 13:48:09 -0400

    add app.jsx content

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+//----- IMPORTS -----//
+import { Outlet } from "react-router-dom";
 
+//CSS
+import 'normalize.css'
+
+//Files
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+
+//----- EXPORTS -----//
 function App() {
 
+  //Return
   return (
-    <>
-
-    </>
+    <div>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </div>
   )
 }
 
